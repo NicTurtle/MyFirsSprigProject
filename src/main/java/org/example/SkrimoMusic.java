1f577ef06cd560c990f2ea4a407bfc6d72c4d0ab
NicTurtle <nic.turtle1012@gmail.com>
2020-12-20 16:18:12 +0200

    Initial commit

---
+++ src/main/java/org/example/SkrimoMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class SkrimoMusic implements Music {
+    @Override
+    public String getSong() {
+        return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    }
+}
---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
